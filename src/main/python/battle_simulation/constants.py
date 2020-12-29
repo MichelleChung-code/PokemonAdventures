340acc30587ffc0765338ec9aaac2bcac8a86cfe
mchung-code <tkd.m.chung@gmail.com>
2020-12-28 20:17:00 -0700

    Add some constants that would be needed for status conditions

--- src/main/python/battle_simulation/constants.py
+++ src/main/python/battle_simulation/constants.py
@@ -6,3 +6,17 @@ DEFENSE = 'Defense'
 SPEED = 'Speed'
 POW = 'power'
 ACC = 'accuracy'
+
+# todo
+# status inflicting conditions
+# https://pokemon.fandom.com/wiki/Status_Effects
+STATUS_EFFECT = 'status'
+PARALYZED = 'paralyzed'
+POISONED = 'poisoned'
+BADLY_POISONED = 'badly poisoned'
+BURNED = 'burned'
+FROZEN = 'frozen'
+FLINCH = 'flinch'  # might need special text here or sounds weird
+CONFUSED = 'confused'
+INFATUATED = 'infatuated'
+LEECH_SEED = 'leech seed' # text could be the same as flinch
