0c4572960c50cb8c6d40d7a2df1bc1fd96d992ad
mchung-code <tkd.m.chung@gmail.com>
2020-12-30 20:21:00 -0700

    Add docstring

--- src/main/python/battle_simulation/battle_common.py
+++ src/main/python/battle_simulation/battle_common.py
@@ -1,5 +1,10 @@
 import logging
 
 def battle_log_msg(msg):
+    """
+    Writes INFO level logging for within a battle.  For maintaining the same logging style.
+    Args:
+        msg: <str> message to log 
+    """
     logging.info('>>> {}'.format(msg))
 
