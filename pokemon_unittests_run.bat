e28ee132563c78b9b0b91be89d6b4c06d1ca7e3a
mchung-code <tkd.m.chung@gmail.com>
2021-01-28 18:03:57 -0700

    Add comment to windows batch file

--- pokemon_unittests_run.bat
+++ pokemon_unittests_run.bat
@@ -1,2 +1,4 @@
+:: For Jenkins CI Set-Up
+
 cd "%WORKSPACE%\src\main\python\"
-python -m unittest discover "%WORKSPACE%\src\main\python\pokemon_unittests" 
+python -m unittest discover "%WORKSPACE%\src\main\python\pokemon_unittests"
