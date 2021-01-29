0b108426ce389102ab20a9564868b92dc266e313
mchung-code <tkd.m.chung@gmail.com>
2021-01-28 17:55:27 -0700

    Rename file for unittests

--- pokemon_unittests_run.bat
+++ pokemon_unittests_run.bat
@@ -1,2 +1,2 @@
 cd "%WORKSPACE%\src\main\python\"
-python -m unittest discover -s "%WORKSPACE%\src\main\python\pokemon_unittests" -p '*_test.py'
+python -m unittest discover "%WORKSPACE%\src\main\python\pokemon_unittests" 
