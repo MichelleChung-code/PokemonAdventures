0464808fdf4f1b6a6e02233d5dfe312b9912e2c4
mchung-code <tkd.m.chung@gmail.com>
2021-06-15 00:02:21 -0600

    Update unittest to check for actual result

--- src/main/python/pokemon_unittests/test_recognize_pokemon.py
+++ src/main/python/pokemon_unittests/test_recognize_pokemon.py
@@ -15,7 +15,5 @@ class ImageRecognitionUnitTests(unittest.TestCase):
 
         x = ImageRecognition(image_dir=False, model_dir=self.model_path, test_img_path=self.test_image_path,
                              run_stored_model_bool=True)
-        x()
-        hello = 'hello'
-        # todo fix model accuracy and then check that it predicts charmander
-        self.assertTrue(hello == 'hello')
+        res_dict = x()
+        self.assertEqual('Charmander', res_dict['FINAL_CHOICE'])
