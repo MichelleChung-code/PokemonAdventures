de9bf230d6591e9afb3abb094855dfd33c798827
mchung-code <tkd.m.chung@gmail.com>
2021-06-16 08:51:44 -0600

    Add new model for starters

--- src/main/python/pokemon_unittests/test_recognize_pokemon.py
+++ src/main/python/pokemon_unittests/test_recognize_pokemon.py
@@ -7,15 +7,15 @@ from pathlib import Path
 class ImageRecognitionUnitTests(unittest.TestCase):
     def setUp(self):
         self.model_path = os.path.join(str(Path(__file__).parents[1]), 'machine_learning', 'recognize_pokemon',
-                                       'model_demo')
+                                       'model_starters')
 
-    def test_recognize_pikachu(self):
-        test_img_path = os.path.join(str(Path(__file__).parents[0]), 'pokemon_unittests_data', 'Pikachu.png')
+    def test_recognize_bulbasaur(self):
+        test_img_path = os.path.join(str(Path(__file__).parents[0]), 'pokemon_unittests_data', 'Bulbasaur.png')
 
         x = ImageRecognition(image_dir=False, model_dir=self.model_path, test_img_path=test_img_path,
                              run_stored_model_bool=True)
         res_dict = x()
-        self.assertEqual('Pikachu', res_dict['FINAL_CHOICE'])
+        self.assertEqual('Bulbasaur', res_dict['FINAL_CHOICE'])
 
     def test_recognize_charmander(self):
         test_img_path = os.path.join(str(Path(__file__).parents[0]), 'pokemon_unittests_data', 'Charmander.png')
@@ -25,10 +25,10 @@ class ImageRecognitionUnitTests(unittest.TestCase):
         res_dict = x()
         self.assertEqual('Charmander', res_dict['FINAL_CHOICE'])
 
-    def test_recognize_blastoise(self):
-        test_img_path = os.path.join(str(Path(__file__).parents[0]), 'pokemon_unittests_data', 'Blastoise.png')
+    def test_recognize_squirtle(self):
+        test_img_path = os.path.join(str(Path(__file__).parents[0]), 'pokemon_unittests_data', 'Squirtle.png')
 
         x = ImageRecognition(image_dir=False, model_dir=self.model_path, test_img_path=test_img_path,
                              run_stored_model_bool=True)
         res_dict = x()
-        self.assertEqual('Blastoise', res_dict['FINAL_CHOICE'])
+        self.assertEqual('Squirtle', res_dict['FINAL_CHOICE'])
